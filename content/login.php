acb603dd1357b1fe60599151c261463386048a49
Lukas Fülling <k3ll4gr0up@gmail.com>
2014-04-01 16:27:00 +0200

    Create login.php

---
+++ content/login.php
@@ -0,0 +1,6 @@
+<form action="content/passwordcheck.php" method="post">
+<input type="text" name="lol_uname" value="Username" />
+<input type="secret" name="lol_passwd" value="Password" />
+<input type="submit" value="login" name="loginbtn" />
+</form>
+<a href="index.php?section=forgot">Forgot Password</a>&nbsp;<a href="index.php?section=register">Register</a>
