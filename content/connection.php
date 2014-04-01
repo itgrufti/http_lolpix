dce7e19a97cde8f0cc538b7604519988700ed6d5
Lukas Fülling <k3ll4gr0up@gmail.com>
2014-04-01 12:25:01 +0200

    Update connection.php

--- content/connection.php
+++ content/connection.php
@@ -1,5 +1,25 @@
 <?php
+  if (file_exists('./config.php')) {
+    
+    include("./config.php");  // Here are the Variables (see config-example.php)
+    
+    $connection=mysql_connect($dbserver,$dbuser,$dbpass);
+      mysql_select_db($dbname) or die ("<h1>0x001255</h1>");                                                                      
+      if(!$connection)
+      {
+        die("<h1>0x001255</h1>");
+      }                                            
+      if(!mysql_select_db($dbname,$connection)) 
+      {                                            
+      	die("<h1>0x001255</h1>");			
+      }
+  }
+  else {
 
-// TODO
+?>
 
+<h1>ERROR: NON EXISTENT DBCONFIG!</h1>
+
+<?php
+  }
 ?>
