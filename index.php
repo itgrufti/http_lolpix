d633c1407b3971aac45b9b5625b7f03e967cfcb7
Lukas Fülling <k3ll4gr0up@gmail.com>
2014-04-01 12:52:46 +0200

    Update index.php

--- index.php
+++ index.php
@@ -7,8 +7,11 @@
 
 	include("content/database.php");
 		// TODO: Set up NSFW
-
+	
+	include("content/header.php");
+	
 	if($section="fresh"){
+		// TODO: Set up NSFW
 		// TODO: Set up database connection.
 		// TODO: Sort images by id descending. 
 		}
@@ -20,6 +23,8 @@
 	elseif($section="about"){
 		// TODO: Write about
 	}
+	
+	include("content/footer.php")M
 
 ?>
 
